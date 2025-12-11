6ac0c342e5a4eddd5442c9dc4d6d5013ac203813
Finn <finnkit05@gmail.com>
2025-12-11 11:16:24 +0100

    Zwischenspeichern
    Mama will los

---
+++ src/views/layout/footer.php
@@ -0,0 +1,13 @@
+<section id="footer">
+    <div class="quickLinks">
+        <p>
+            Menu:
+            <a href="/">Start</a>| 
+            <a href="/impressum/">Kontakt</a>
+        </p>
+        <p>  
+            <a href="/impressum/">Impressum</a>
+        </p>
+        <p>&copy; <?= date("Y") ?> Quint Felicity</p>
+    </div>
+</section>
---
+++ src/views/topBar.php
@@ -0,0 +1,7 @@
+<div id="topbarBg"></div>
+<div id="topbar">
+    <ichoutplayeflex></ichoutplayeflex>
+    <img src="/resources/LogoRotx.png" alt="Logo">
+    <svg class="menuIcon" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/></svg>
+    <svg class="menuIcon null" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+</div>

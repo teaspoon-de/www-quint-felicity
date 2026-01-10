7106b9978e28422bd69ea94103872c609af2cb64
Finn <finnkit05@gmail.com>
2026-01-10 02:31:07 +0100

    Manifest wieder raus

--- src/Views/layout/headers.php
+++ src/Views/layout/headers.php
@@ -6,7 +6,6 @@
 	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 	<link rel="shortcut icon" href="/favicon.ico" />
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-	<link rel="manifest" href="/site.webmanifest" />
 
 	<link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/footer.css">

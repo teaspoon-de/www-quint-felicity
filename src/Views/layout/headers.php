72867ed506dc172df66ff20ede5711cfa8e8ee6c
Finn <finnkit05@gmail.com>
2026-01-10 02:15:56 +0100

    Favicon Update

--- src/Views/layout/headers.php
+++ src/Views/layout/headers.php
@@ -2,7 +2,12 @@
 	<meta charset="UTF-8" />
 	<title><?= $pageTitle?></title>
 
-    <link rel="icon" type="" href="https://quint-felicity.de/resources/LogoRotICO.png">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+
 	<link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/css/topbar.css">

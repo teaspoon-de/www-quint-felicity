5f4806ea4ad7e4258f84d1c6f3d6c5a4592028d0
Finn <finnkit05@gmail.com>
2026-02-13 20:48:16 +0100

    Impressum Änderung

--- src/Views/pages/impressum.php
+++ src/Views/pages/impressum.php
@@ -2,13 +2,13 @@
     <article>
         <h1>Angaben gemäß § 5 TMG:</h1>
         <p>Quint Felicity<br>
-        Vertreten durch: Alexander Buss<br> 
-        Anxbachstraße 16<br>
-        53562 Sankt Katharinen</p>
+        Vertreten durch: Finn Kittelmann<br> 
+        Dürener Straße 126<br>
+        50931 Köln</p>
         <p>Kontakt:<br>
         E-Mail: info@quint-felicity.de</p>
         <p>Verantwortlich für den Inhalt nach § 18 Abs. 2 MStV:<br>
-        Alexander Buss, Anxbachstraße 16, 53562 Sankt Katharinen</p>
+        Finn Kittelmann, Dürener Straße 126, 50931 Köln</p>
 
         <h1>Haftungsausschluss:</h1>
         <p>Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links.<br>
